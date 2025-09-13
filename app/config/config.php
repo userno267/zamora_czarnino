28fed34ef9d6b4ff17bf72308c536162cf7f7d22
Your Name <you@example.com>
2025-09-13 09:46:55 +0800

    First commit

--- app/config/config.php
+++ app/config/config.php
@@ -90,7 +90,7 @@ $config['base_url'] 				= 'https://zamora-czarnino.onrender.com';
 | variable to blank.
 |
 */
-$config['index_page']               = 'index.php';
+$config['index_page']               = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -128,7 +128,7 @@ $config['log_dir']                  = 'runtime/logs/';
 | Note: This will NOT disable or override the LavaLust-specific
 |	autoloading (app/config/autoload.php)
 */
-$config['composer_autoload']        = FALSE;
+$config['composer_autoload']        = true;
 
 /*
 |--------------------------------------------------------------------------
